743e03e1107a2651092c6f6804bd317402bb8d49
KUGA16 <gachan_1216@yahoo.co.jp>
2020-04-10 15:41:53 +0000

    Add: リレーションシップモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_10_153904) do
+ActiveRecord::Schema.define(version: 2020_04_10_154103) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 2020_04_10_153904) do
   create_table "posts", force: :cascade do |t|
     t.integer "account_id"
     t.text "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "following_id"
+    t.integer "follower_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
