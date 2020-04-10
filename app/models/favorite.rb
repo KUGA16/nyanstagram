db52fdcd55fb0481cf1b209e129d512411d7b586
KUGA16 <gachan_1216@yahoo.co.jp>
2020-04-10 16:06:42 +0000

    Add: リレーションを記述

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,6 @@
 class Favorite < ApplicationRecord
+
+  belongs_to :account
+  belongs_to :post
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+
+  belongs_to :account
+  has_many :favorites,   dependent: :destroy
+  has_many :post_images, dependent: :destroy
+  has_many :post_tags,   dependent: :destroy
+  has_many :tags,        through: :post_tags
+
 end
