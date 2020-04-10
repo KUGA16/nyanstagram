106c58ae05500108e6e554a1aa21ceed2ae67d97
KUGA16 <gachan_1216@yahoo.co.jp>
2020-04-10 15:43:21 +0000

    Add: post_tagモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_10_154103) do
+ActiveRecord::Schema.define(version: 2020_04_10_154239) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 2020_04_10_154103) do
   create_table "post_images", force: :cascade do |t|
     t.integer "post_id"
     t.string "image", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_tags", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
